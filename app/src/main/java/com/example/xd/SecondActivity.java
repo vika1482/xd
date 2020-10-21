5c9ed06b1b7c505013b33186f65dd62bbfe7934a
Atikeeva Vika <vika1482@mail.ru>
2020-10-21 23:37:41 +0700

    переход на второй экран и обратно, при нажатии на иконку домой

---
+++ app/src/main/java/com/example/xd/SecondActivity.java
@@ -0,0 +1,5 @@
+package com.example.xd;
+
+public class SecondActivity {
+
+}
---
+++ app/src/main/java/com/example/xd/activity_second.java
@@ -0,0 +1,26 @@
+package com.example.xd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class activity_second extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second2);
+        ImageView imageView12 = (ImageView) findViewById(R.id.imageView12);
+        imageView12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent_1 = new Intent( activity_second.this,MainActivity.class);
+                startActivity(intent_1);
+            }
+        } );
+    }
+}

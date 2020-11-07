5c1eda98cb8683f8ab7fcf0431bc65a92de823d0
Atikeeva Vika <vika1482@mail.ru>
2020-11-07 18:07:54 +0700

    Реализация камеры

--- app/src/main/java/com/example/xd/activity_second.java
+++ app/src/main/java/com/example/xd/activity_second.java
@@ -1,15 +1,18 @@
 package com.example.xd;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
-import android.widget.Button;
+
 import android.widget.ImageView;
 
 public class activity_second extends AppCompatActivity {
-
+private static final int REQUEST_CODE = 1313;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,5 +33,23 @@ public class activity_second extends AppCompatActivity {
                 startActivity(intent_1);
             }
         } );
+
+        ImageView imageView8 = (ImageView) findViewById(R.id.imageView8);
+        imageView8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,REQUEST_CODE);
+            }
+        } );
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (REQUEST_CODE == requestCode)
+        {
+            Bitmap bit = (Bitmap) data.getExtras().get("data");
+        }
     }
 }
--- app/src/main/java/com/example/xd/activity_third.java
+++ app/src/main/java/com/example/xd/activity_third.java
@@ -1,35 +1,56 @@
 package com.example.xd;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
-import android.widget.Button;
+
 import android.widget.ImageView;
 
 public class activity_third extends AppCompatActivity {
+    private static final int REQUEST_CODE = 1313;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_third);
+            ImageView imageView21 = (ImageView) findViewById(R.id.imageView21);
+            imageView21.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent_2 = new Intent( activity_third.this,MainActivity.class);
+                    startActivity(intent_2);
+                }
+            } );
+
+            ImageView imageView24 = (ImageView) findViewById(R.id.imageView24);
+            imageView24.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent_3 = new Intent( activity_third.this,activity_second.class);
+                    startActivity(intent_3);
+                }
+            } );
+
+            ImageView imageView22 = (ImageView) findViewById(R.id.imageView22);
+            imageView22.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, REQUEST_CODE);
+                }
+            } );
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_third);
-        ImageView imageView21 = (ImageView) findViewById(R.id.imageView21);
-        imageView21.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent_2 = new Intent( activity_third.this,MainActivity.class);
-                startActivity(intent_2);
-            }
-        } );
-
-        ImageView imageView24 = (ImageView) findViewById(R.id.imageView24);
-        imageView24.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent_3 = new Intent( activity_third.this,activity_second.class);
-                startActivity(intent_3);
-            }
-        } );
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (REQUEST_CODE == requestCode)
+        {
+            Bitmap bit = (Bitmap) data.getExtras().get("data");
+        }
     }
 }
